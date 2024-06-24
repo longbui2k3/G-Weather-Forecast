14f19a2dafda1df1a900a8e3c3f2ffd7b3922a51
longbui2k3 <buiduclongit@gmail.com>
2024-06-24 13:31:25 +0700

    Fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CLIENT_ORIGIN } from './utils/globalVariables';
-import { NextFunction, Request, Response } from 'express';
-import express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(express.json());
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+  });
+
   const options = {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
