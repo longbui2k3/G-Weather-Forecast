4c924f54c2299f5e63e4553d8a83a28bbb85a98c
longbui2k3 <buiduclongit@gmail.com>
2024-06-24 11:46:00 +0700

    Fix sth'

--- client/src/components/Subscribe.js
+++ client/src/components/Subscribe.js
@@ -16,6 +16,7 @@ export function Subscribe() {
           Accept: "application/json",
           "Content-Type": "application/json;charset=UTF-8",
           "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true",
         },
         body: JSON.stringify({
           email,
