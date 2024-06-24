375b1962b5a078c7391fc4e976193b3d3fabfdc3
longbui2k3 <buiduclongit@gmail.com>
2024-06-24 14:09:29 +0700

    Fix

--- client/src/api/createHistory.js
+++ client/src/api/createHistory.js
@@ -3,7 +3,9 @@ import { API_ORIGIN } from "../utils/globalVariable";
 
 export const createHistory = async (body) => {
   try {
-    const res = await axios.post(`${API_ORIGIN}/api/v1/history`, body);
+    const res = await axios.post(`${API_ORIGIN}/api/v1/history`, body, {
+      withCredentials: false,
+    });
     return res.data;
   } catch (err) {
     console.log(err);
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import bodyParser from 'body-parser';
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    rawBody: true,
-  });
-  app.use(bodyParser.json({ type: 'application/json' }));
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const options = {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
