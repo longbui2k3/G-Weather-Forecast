431a9fc29a39811a4ab213c6d0ea33219a6aa211
longbui2k3 <buiduclongit@gmail.com>
2024-06-24 12:29:01 +0700

    Fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,21 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CLIENT_ORIGIN } from './utils/globalVariables';
-import cors from 'cors-ts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   allowedHeaders: ['content-type'],
-  //   origin: CLIENT_ORIGIN,
-  //   credentials: true,
-  // });
-  app.use(
-    cors({
-      origin: CLIENT_ORIGIN,
-      optionsSuccessStatus: 200,
-    }),
-  );
+  var whitelist = [CLIENT_ORIGIN];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(4000);
 }
 bootstrap();
