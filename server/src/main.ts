95f99a7c798620baf4c130aff0d8e8b29de87824
longbui2k3 <buiduclongit@gmail.com>
2024-06-24 11:59:36 +0700

    Fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CLIENT_ORIGIN } from './utils/globalVariables';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    origin: CLIENT_ORIGIN + '/',
+    methods: ['GET', 'POST'],
   });
   await app.listen(4000);
 }
